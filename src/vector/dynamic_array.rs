7dbaf3eda5b41b697483b13be8a26b4e7f2effe7
杨文博 <merin@outlook.com>
2024-06-20 20:58:04 +0800

    feat: add vector

---
+++ src/vector/dynamic_array.rs
@@ -0,0 +1,14 @@
+fn borrow_element_same_time() {
+    let mut v = vec![1, 2, 3];
+    for i in &v {
+        println!("{}", i)
+    }
+    v.push(1);
+    println!("{:?}", v);
+
+    let mut v1 = vec![1, 2, 3, 4, 5];
+    // let first =  &v1[0];// error: 放在push之前执行报错,不可变借用发生在此处
+    let first = v1[0];
+    v1.push(6);
+    println!("The first element is: {first}");
+}
---
+++ src/vector/vec_method.rs
@@ -0,0 +1,39 @@
+pub fn invoke() {
+    let vec1 = vec![0; 3];
+    let vec2 = Vec::from([0, 0, 0]);
+    assert_eq!(vec1, vec2);
+    let mut v = Vec::with_capacity(10);
+    v.extend([1, 2, 3]);    // 附加数据到 v
+    println!("Vector 长度是: {}, 容量是: {}", v.len(), v.capacity());
+
+    v.reserve(100);        // 调整 v 的容量，至少要有 100 的容量
+    println!("Vector（reserve） 长度是: {}, 容量是: {}", v.len(), v.capacity());
+
+    v.shrink_to_fit();     // 释放剩余的容量，一般情况下，不会主动去释放容量
+    println!("Vector（shrink_to_fit） 长度是: {}, 容量是: {}", v.len(), v.capacity());
+
+    assert!(!v.is_empty());
+    v.insert(2,4);
+    println!("v：{:?}", v);
+    assert_eq!(v.remove(2), 4);
+    assert_eq!(v.pop(), Some(3));
+    assert_eq!(v.pop(), Some(2));
+    assert_eq!(v.pop(), Some(1));
+    assert_eq!(v.pop(), None);
+    v.clear();
+    let mut vec3 = [111, 333].to_vec();
+    v.append(&mut vec3); // ************** append导致vec3清空数据  ****************
+    //  self.append_elements(other.as_slice() as _);
+    //  other.set_len(0);
+    v.truncate(1);   // 截断到指定长度
+
+    v.retain( |x| *x > 10); // 保留满足条件的元素
+
+    println!("v: {:?}", v);
+
+    let mut vec4 = vec![111, 222, 333, 444, 555, 666];
+    let mut m: Vec<_> = vec4.drain(1..=3).collect(); //删除指定范围的元素，同时获取被删除元素的迭代器
+    let vec5 = m.split_off(1);
+    println!("vec4: {:?}", vec4);
+    println!("vec5: {:?}", vec5)
+}
