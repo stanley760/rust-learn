5cb7f0c8cf3dec811b5c5ae73b1c512c056a97e8
ywb <merin@outlook.com>
2024-08-07 17:56:28 +0800

    fixed: add icon

--- process_killer/src/main.rs
+++ process_killer/src/main.rs
@@ -1,14 +1,22 @@
 use dioxus::desktop::{Config, WindowBuilder};
+use dioxus::desktop::tao::window::Icon;
 use dioxus::prelude::*;
 
 use process_killer::ui::draw::app;
 
 fn main() {
+    let icon_path = "process_killer/src/assets/icon.png";
+    let icon = image::open(icon_path).expect("Failed to load icon file");
+
+    let window = WindowBuilder::new()
+        .with_title("process killer")
+        .with_window_icon(Some(Icon::from_rgba(icon.to_rgba8().into_raw(), icon.width(), icon.height()).unwrap()))
+        .with_resizable(true);
+
+
     LaunchBuilder::desktop()
-        .with_cfg(Config::new().with_window(WindowBuilder::new()
-                .with_title("process killer")
-                .with_resizable(true)
-            )
-        ).launch(app)
+        .with_cfg(Config::new().with_window(window))
+        .launch(app)
 }
+
 
