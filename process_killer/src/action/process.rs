b88c81556203d7be33d76bb6eb74d1844f55eb0e
ywb <merin@outlook.com>
2024-08-12 17:26:53 +0800

    fixed: add events.

--- process_killer/src/action/process.rs
+++ process_killer/src/action/process.rs
@@ -34,6 +34,28 @@ impl Process {
         }).skip(1).collect()
     }
 
+    pub fn search(port: &str) -> Result<Vec<Self>, Error> {
+        let port = port.parse::<u32>().map_err(|_| Error::new(ErrorKind::InvalidInput, "it's not a number"))?;
+        let mut result = Self::run();
+        result.retain(|x| {
+            let inner_host = &x.inner_host;
+            let port_str = if inner_host.starts_with('[') {
+                &inner_host[inner_host.find("]:").unwrap() + 2..]
+            } else {
+                &inner_host[inner_host.find(':').unwrap() + 1..]
+            };
+            match port_str.parse::<u32>() {
+                Ok(parsed_port) => parsed_port == port,
+                Err(_) => false,
+            }
+        });
+        if result.is_empty() {
+            return Err(Error::new(ErrorKind::NotFound, "not found"));
+        }
+        
+        Ok(result)
+    }
+
     pub fn kill(pid: &str) -> Result<(), Error> {
         
         let pid = pid.parse::<u32>().map_err(|_| Error::new(ErrorKind::InvalidInput, "it's not a number"))?;
@@ -55,7 +77,7 @@ impl Process {
         }
     }
 
-    fn hanle_cross_operate_system(parts: Vec<&str>) -> Option<Process> {
+    fn hanle_cross_operate_system(parts: Vec<&str>) -> Option<Self> {
         let windows_flag = cfg!(target_os = "windows");
         let macos_flag = cfg!(target_os = "macos");
         if (parts.len() < 5 && windows_flag) || (macos_flag && parts.len() < 8) {
@@ -67,7 +89,7 @@ impl Process {
         let status = if macos_flag { parts[5].to_string() } else { parts[3].to_string() };
         let pid = if macos_flag { parts[8].to_string() } else { parts[4].to_string() };
 
-        Some(Process {
+        Some(Self {
             protocol,
             inner_host,
             outer_host,
--- process_killer/src/ui/draw.rs
+++ process_killer/src/ui/draw.rs
@@ -3,10 +3,12 @@ use crate::action::process::Process;
 
 pub fn app() -> Element {
     let header_txt : [&str; 5] = ["protocol", "inner_host", "outer_host", "status", "pid",];
-    
-    let datas: Vec<Vec<String>> = Process::run().iter().map(|x| {
+    let init_datas: Vec<Vec<String>> = Process::run().iter().map(|x| {
         vec![x.protocol.clone(), x.inner_host.clone(), x.outer_host.clone(), x.status.clone(), x.pid.clone()]
     }).collect();
+    let mut datas = use_signal(|| init_datas);
+    let mut port_value = use_signal(|| String::new());
+    let mut pid_value = use_signal(|| String::new());
     
     rsx!(
         title { "Process Killer" }
@@ -16,13 +18,30 @@ pub fn app() -> Element {
             div {
                 class: "div-form",
                 label { form: "port-label", class: "port", "port:"}
-                input { form: "port-input", name: "port-input", class: "port"}
-                button { form: "btn-search", name: "btn-search", "search"}
-                button { form: "btn-reset", name: "btn-reset", "reset" }
+                input { form: "port-input", name: "port-input", value: "{port_value}"
+                    , oninput: move |event| {
+                    port_value.set(event.value().clone())
+                }, class: "port"}
+                button { form: "btn-search", name: "btn-search", onclick: move |_| {
+                    let port_str = port_value.read();
+                    let data = Process::search(port_str.as_str()).unwrap();
+                    datas.set(data.iter().map(|x| {
+                        vec![x.protocol.clone(), x.inner_host.clone(), x.outer_host.clone(), x.status.clone(), x.pid.clone()]
+                    }).collect());
+                }, "search"}
+                button { form: "btn-reset", name: "btn-reset", onclick: move |_| {
+                    port_value.set(String::new()); pid_value.set(String::new())
+                }, "reset" }
                 
                 label { form: "pid-label", "pid:"}
-                input { form: "pid-input", name: "pid-input", class: "pid"}
-                button { form: "btn-kill", name: "btn-kill", "kill"}
+                input { form: "pid-input", name: "pid-input",value: "{pid_value}",
+                    oninput: move |event| {
+                        pid_value.set(event.value().clone())
+                    }, class: "pid"}
+                button { form: "btn-kill", name: "btn-kill", onclick: move |_| {
+                    let pid_str = pid_value.read();
+                    Process::kill(pid_str.as_str()).unwrap()
+                }, "kill"}
             }
             
             table {
