63c596b8e9af709380da2130daa2e731d2b35c0b
stanley <merin@outlook.com>
2024-12-11 00:44:44 +0800

    feat: optimized codes.

---
+++ rcli/src/operation/genpwd.rs
@@ -0,0 +1,19 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+pub struct GenPwdOpts {
+    #[arg(long, default_value_t = 16)]
+    pub length: u8,
+
+    #[arg(long, default_value_t = true)]
+    pub lowercase: bool,
+
+    #[arg(long, default_value_t = true)]
+    pub uppercase: bool,
+
+    #[arg(long, default_value_t= true)]
+    pub numbers: bool,
+
+    #[arg(long, default_value_t= true)]
+    pub symbols: bool,
+}
