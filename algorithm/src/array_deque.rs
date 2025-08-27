a88d4e534d272f154833389fb60f598924dd27bb
ywb <merin@outlook.com>
2025-08-27 17:48:52 +0800

    feat(array_deque): implement doubly linked list deque with full functionality
    - Add Default and Drop trait implementations for ArrayDeque
    - Implement push_back, pop_back, and clear methods
    - Add peek_front, peek_back, peek_front_mut, and peek_back_mut methods
    - Provide get method with bounds checking
    - Implement remove by index with proper node linking
    - Add remove_value method to remove element by value
    - Implement len and is_empty utility methods
    - Fix node linking logic in push_front and push_back methods

--- algorithm/src/array_deque.rs
+++ algorithm/src/array_deque.rs
@@ -1,9 +1,7 @@
 use std::{marker::PhantomData, ptr::NonNull};
 
-
-
 pub struct ArrayDeque<T> {
-    front:  Link<T>,
+    front: Link<T>,
     back: Link<T>,
     len: usize,
     _boo: PhantomData<T>,
@@ -14,9 +12,21 @@ type Link<T> = Option<NonNull<Node<T>>>;
 struct Node<T> {
     front: Link<T>,
     back: Link<T>,
-    elem: T, 
+    elem: T,
 }
-// todo unimplemented some function.
+
+impl<T> Default for ArrayDeque<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T> Drop for ArrayDeque<T> {
+    fn drop(&mut self) {
+        self.clear();
+    }
+}
+
 impl<T> ArrayDeque<T> {
     pub fn new() -> Self {
         Self {
@@ -27,13 +37,22 @@ impl<T> ArrayDeque<T> {
         }
     }
 
+    /// Returns the length of the linked list
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len == 0
+    }
+
     pub fn push_front(&mut self, elem: T) {
         unsafe {
             let new = NonNull::new_unchecked(Box::into_raw(Box::new(Node {
-                    front: None,
-                    back: None,
-                    elem,
-                })));
+                front: None,
+                back: None,
+                elem,
+            })));
             if let Some(old) = self.front {
                 (*old.as_ptr()).front = Some(new);
                 (*new.as_ptr()).back = Some(old);
@@ -62,6 +81,118 @@ impl<T> ArrayDeque<T> {
             })
         }
     }
+
+    pub fn push_back(&mut self, elem: T) {
+        unsafe {
+            let new = NonNull::new_unchecked(Box::into_raw(Box::new(Node {
+                front: None,
+                back: None,
+                elem,
+            })));
+            if let Some(old) = self.back {
+                (*old.as_ptr()).back = Some(new);
+                (*new.as_ptr()).front = Some(old);
+            } else {
+                self.front = Some(new);
+            }
+            self.back = Some(new);
+            self.len += 1;
+        }
+    }
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.back.take().map(|node| unsafe {
+            let boxed_node = Box::from_raw(node.as_ptr());
+            let result = boxed_node.elem;
+
+            self.back = boxed_node.front;
+
+            if let Some(new) = self.back {
+                (*new.as_ptr()).back = None;
+            } else {
+                self.front = None;
+            }
+            self.len -= 1;
+            result
+        })
+    }
+
+    pub fn clear(&mut self) {
+        while self.pop_front().is_some() {}
+    }
+
+    pub fn peek_front(&mut self) -> Option<&T> {
+        self.front.map(|node| unsafe { &(*node.as_ptr()).elem })
+    }
+
+    pub fn peek_back(&mut self) -> Option<&T> {
+        self.back.map(|node| unsafe { &(*node.as_ptr()).elem })
+    }
+
+    pub fn peek_front_mut(&mut self) -> Option<&mut T> {
+        self.front.map(|node| unsafe { &mut (*node.as_ptr()).elem })
+    }
+
+    pub fn peek_back_mut(&mut self) -> Option<&mut T> {
+        self.back.map(|node| unsafe { &mut (*node.as_ptr()).elem })
+    }
+
+    pub fn get(&self, index: usize) -> Option<&T> {
+        if index >= self.len {
+            panic!("Index out of bounds");
+        }
+        unsafe {
+            (0..index)
+                .try_fold(self.front?, |cur, _| (*cur.as_ptr()).back)
+                .map(|cur| &(*cur.as_ptr()).elem)
+        }
+    }
+
+    pub fn remove(&mut self, index: usize) -> Option<T> {
+        if index >= self.len {
+            panic!("Index out of bounds");
+        }
+
+        if index == 0 {
+            self.pop_front()
+        } else if index == self.len - 1 {
+            self.pop_back()
+        } else {
+            unsafe {
+                let current = (0..index)
+                    .try_fold(self.front.unwrap(), |cur, _| (*cur.as_ptr()).back)
+                    .unwrap();
+
+                let prev = (*current.as_ptr()).front.unwrap();
+                let next = (*current.as_ptr()).back.unwrap();
+
+                (*prev.as_ptr()).back = Some(next);
+                (*next.as_ptr()).front = Some(prev);
+
+                let boxed_node = Box::from_raw(current.as_ptr());
+                self.len -= 1;
+                Some(boxed_node.elem)
+            }
+        }
+    }
+
+    pub fn remove_value(&mut self, value: &T) -> bool 
+    where 
+        T: PartialEq,
+    {
+        unsafe {
+            let mut current = self.front;
+            let mut index = 0;
+
+            while let Some(node) = current { 
+                if (*node.as_ptr()).elem == *value {
+                    self.remove(index);
+                    return true;
+                }
+                current = (*node.as_ptr()).back;
+                index += 1;
+            }
+        }
+        false
+    }
 }
-
-
