22c8b2f1af7bf8afddf42cbb1565ec476b982d0e
ywb <merin@outlook.com>
2025-12-02 09:48:56 +0800

    feat: optimized some code

--- algorithm/src/binary/search_33.rs
+++ algorithm/src/binary/search_33.rs
@@ -3,28 +3,40 @@ struct Solution;
 #[allow(dead_code)]
 impl Solution {
     fn find_min(nums: &[i32]) -> usize {
-        let mut left = 0;
-        let mut right = nums.len() - 1; // 左闭右开区间 [0, n-1)
-        while left < right {
-            // 区间不为空
-            let mid = left + (right - left) / 2;
-            if nums[mid] < nums[nums.len() - 1] {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        left
+        // let mut left = 0;
+        // let mut right = nums.len() - 1; // 左闭右开区间 [0, n-1)
+        // while left < right {
+        //     // 区间不为空
+        //     let mid = left + (right - left) / 2;
+        //     if nums[mid] < nums[nums.len() - 1] {
+        //         right = mid;
+        //     } else {
+        //         left = mid + 1;
+        //     }
+        // }
+        // left
+
+        let n = nums.len();
+        let pos = (0..n - 1)
+            .collect::<Vec<_>>()
+            .partition_point(|&i| nums[i] >= nums[nums.len() - 1]);
+        pos
     }
 
     // 有序数组中找 target 的下标
-    fn lower_bound(nums: &[i32],left: usize, target: i32) -> i32 {
-       
-        let index = nums.partition_point(|&x| x < target);
+    fn lower_bound(nums: &[i32], mut left: usize, mut right: usize, target: i32) -> i32 {
+        while left < right {
+            let mid =  (left + right) >> 1;
+            if nums[mid] < target {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
 
         // 检查找到的位置是否是目标值
-        if index < nums.len() && nums[index] == target {
-            (left + index) as i32
+        if nums[left] == target {
+            left as i32
         } else {
             -1
         }
@@ -34,10 +46,10 @@ impl Solution {
         let i = Self::find_min(&nums);
         if target > nums[nums.len() - 1] {
             // target 在第一段
-            Self::lower_bound(&nums[0..i], 0, target) // 左闭右开区间 [0, i)
+            Self::lower_bound(&nums ,0, i, target) // 左闭右开区间 [0, i)
         } else {
             // target 在第二段
-            Self::lower_bound(&nums[i..nums.len()], i, target) // 左闭右开区间 [i, n)
+            Self::lower_bound(&nums, i, nums.len(), target) // 左闭右开区间 [i, n)
         }
     }
 }
