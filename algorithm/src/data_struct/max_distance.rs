d019e93a8106d76753c4d81b5a7d6158e8fda08a
stanley <merin@outlook.com>
2026-01-23 16:32:13 +0800

    refactor(max_distance): improve code comments for clarity on min and max value tracking

--- algorithm/src/data_struct/max_distance.rs
+++ algorithm/src/data_struct/max_distance.rs
@@ -10,11 +10,15 @@ impl Solution {
         let mut min = i32::MAX / 2;
         let mut max = i32::MIN /2 ;
         for a in arrays {
+            // - 每个数组的最小值是 a[0]
+            // - 每个数组的最大值是 a[a.len() - 1]
             let x = a[0];
             let y = a[a.len() - 1];
 
             ans = ans.max(y- min).max(max - x);
+            // 记录上一个数组中最小的值和当前较小值中最小的值
             min = min.min(x);
+            // 记录上一个数组中最大的和当前较大值中最大的值
             max = max.max(y);
         }
         ans
