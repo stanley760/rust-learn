2735f31845c4531147e5b727e4c04653cb3f2d97
stanley <merin@outlook.com>
2026-07-03 05:06:43 +0800

    feat: add futures dependency and refactor chat completion functionality for streaming responses

--- defined-agent/src/main.rs
+++ defined-agent/src/main.rs
@@ -1,6 +1,5 @@
-use anyhow::Ok;
-
 use defined_agent::llm::complete;
+use futures::StreamExt;
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
 
@@ -11,16 +10,30 @@ async fn main() -> anyhow::Result<()> {
     let key = std::env::var("OPENAI_BASE_URL")?;
     println!("{:?}", key);
     let subscriber = FmtSubscriber::builder()
-                    .with_max_level(Level::INFO)
-                    .finish();
+        .with_max_level(Level::INFO)
+        .finish();
     tracing::subscriber::set_global_default(subscriber)?;
-
 
     let result = complete::chat_complete_structed(
         CHAT_GPT_120,
-        Some("你是一个旅游博主"),
-        "我要在最近准备从成都到九寨沟旅游，给出一个详细的旅游攻略").await?;
-    println!("{result:#?}");
+        Some("你是一个天文学家"),
+        "请你帮我写一篇关于黑洞的科普文章，要求内容通俗易懂，适合大众阅读。",
+    );
+
+    futures::pin_mut!(result);
+    let mut output = String::new();
+    while let Some(item) = result.next().await {
+        match item {
+            Result::Ok(plan) => {
+                output.push_str(&plan);
+                print!("{}", plan);
+            }
+            Err(e) => {
+                eprintln!("Error receiving streaming: {:?}", e);
+                return Err(e);
+            }
+        }
+    }
+    println!("--------------------------------");
     Ok(())
 }
- 
